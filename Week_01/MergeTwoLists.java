3ac17c04b85c4ca69d70419d9aa556de293bd330
gaoqian <wb-zhangliqian@zhongan.io>
2021-02-04 11:10:20 +0800

    合并两个链表

---
+++ Week_01/MergeTwoLists.java
@@ -0,0 +1,27 @@
+/**合并两个链表
+ 这么难吗，第一次竟然想不清楚
+ 是为什么
+ */
+
+public class MergeTwoLists {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }else if (l2 == null) {
+            return l1;
+        }else if (l1.val > l2.val) {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        } else {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+    }
+}
