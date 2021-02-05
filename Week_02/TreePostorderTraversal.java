d2752a9a38a19668a0f85ac832a7b55cb5d1b6ee
gaoqian <wb-zhangliqian@zhongan.io>
2021-02-06 01:41:46 +0800

    N叉树的后序遍历

---
+++ Week_02/TreePostorderTraversal.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 15000
+
+ */
+public class TreePostorderTraversal {
+    List<Integer> list;
+    public List<Integer> postorder(Node root) {
+        list = new ArrayList<>();
+        order(root);
+        return list;
+    }
+
+    private void order(Node root) {
+        if (root == null) {
+            return;
+        }
+        for(Node child:root.children) {
+            order(child);
+        }
+        list.add(root.val);
+    }
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+}
