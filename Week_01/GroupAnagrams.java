cf03bb3d9f0a26c42204ab41d7f8030078aa4992
gaoqian <wb-zhangliqian@zhongan.io>
2021-02-05 19:19:24 +0800

    字母异位词分组

--- Week_01/GroupAnagrams.java
+++ Week_01/GroupAnagrams.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 /**
 
  */
 public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list =  map.getOrDefault(key,new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
 }
