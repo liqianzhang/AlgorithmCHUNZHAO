317dc1a6c83b0bc1adb43245515bcd2154bba8c8
gaoqian <wb-zhangliqian@zhongan.io>
2021-02-07 11:22:43 +0800

    买卖股票最佳时机

---
+++ Week_03/BestSellStock.java
@@ -0,0 +1,17 @@
+/**
+ * @MethodName: $
+ * @Description: TODO
+ * @Param: $
+ * @Return: $
+ * @Author: zhangliqian
+ * @Date: $
+ */
+public class BestSellStock {
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        for (int i = 1; i < prices.length; i++) {
+            ans += Math.max(0, prices[i] - prices[i-1]);
+        }
+        return ans;
+    }
+}
